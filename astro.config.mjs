666baa4c4ae28aa25eda19b68676f1355bcb4118
Sankalpa Adhikari <sankalpa2522@gmail.com>
2025-12-05 00:18:14 +0545

    fix:changed to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nsae-nepal.github.io",
+  site: "http://nsae.org.np",
   integrations: [mdx(), sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
